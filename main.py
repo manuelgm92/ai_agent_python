8f1b6074cba2c4443b85b6f50c7186b4b58b4ea8
manuelgm92 <106698468+manuelgm92@users.noreply.github.com>
2026-09-17 11:49:28 +0100

    update main.py

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-def main():
-    print("Hello from ai-agent-python!")
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
 
+load_dotenv()
+api_key = os.environ.get("OPENROUTER_API_KEY")
 
-if __name__ == "__main__":
-    main()
+if api_key == None:
+    raise RuntimeError("Runtime error")
+
+client = OpenAI(
+    base_url = "https://openrouter.ai/api/v1",
+    api_key=api_key,
+)
+
+response = client.chat.completions.create(
+    model="openrouter/free",
+    messages=[
+        {
+            "role": "user",
+            "content": "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        }
+    ]
+)
+
+print(response.choices[0].message.content)
